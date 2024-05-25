c5a2f465d4738bd8aee0dda9e14592667921f997
Mohit Pardhi <mpardhi11@gmail.com>
2024-05-25 16:22:45 +0530

    feat: Add user model for storing user information

---
+++ server/src/models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  emailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  passwordSalt: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+export const User = mongoose.model('User', userSchema);
+// Path: server/src/controllers/userController.js
